09b3afbd99e46e196e7c9027ceb42fcdeacac58e
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-03-28 01:49:05 +0330

    implemented class AccountServiceImpl

---
+++ src/main/java/com/mmf/financeflow/service/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mmf.financeflow.service;
+
+import com.mmf.financeflow.entity.Account;
+import com.mmf.financeflow.repository.AccountRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public Account create(Account account) {
+        return accountRepository.save(account);
+    }
+
+    @Override
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
+}
