f1d594b1de46da3f8dfeab02c81f7bda623d53dd
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-03-28 01:51:36 +0330

    implemented class ExpenseServiceImpl

---
+++ src/main/java/com/mmf/financeflow/service/ExpenseServiceImpl.java
@@ -0,0 +1,26 @@
+package com.mmf.financeflow.service;
+
+import com.mmf.financeflow.entity.Expense;
+import com.mmf.financeflow.repository.ExpenseRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class ExpenseServiceImpl implements ExpenseService {
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @Override
+    public Expense create(Expense expense) {
+        return expenseRepository.save(expense);
+    }
+
+    @Override
+    public List<Expense> findAll() {
+        return expenseRepository.findAll();
+    }
+}
