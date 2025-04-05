a822f13d9ab9566250aea51be5a919b3255497b5
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-05 22:50:16 +0330

    implemented service AppUserDetailsService

---
+++ src/main/java/com/mmf/financeflow/service/AppUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.mmf.financeflow.service;
+
+import com.mmf.financeflow.entity.AppUser;
+import com.mmf.financeflow.entity.UserRole;
+import com.mmf.financeflow.repository.AppUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser appUser = appUserRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found!"));
+
+        Set<SimpleGrantedAuthority> authorities = appUser.getRoles().stream()
+                .map(UserRole::name)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                appUser.getUsername(),
+                appUser.getPassword(),
+                authorities);
+    }
+}
