d1a20a0a511e418bc580161ddb001306e457b32f
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-05 22:39:28 +0330

    created entity AppUser

---
+++ src/main/java/com/mmf/financeflow/entity/AppUser.java
@@ -0,0 +1,29 @@
+package com.mmf.financeflow.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "app_user")
+@Getter
+@Setter
+public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "roles")
+    private Set<UserRole> roles;
+}
---
+++ src/main/java/com/mmf/financeflow/entity/UserRole.java
@@ -0,0 +1,6 @@
+package com.mmf.financeflow.entity;
+
+public enum UserRole {
+    ROLE_USER,
+    ROLE_ADMIN,
+}
