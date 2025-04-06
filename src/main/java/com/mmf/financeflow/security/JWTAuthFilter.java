541a6f39daf6d496ab89e620c2a54386773eeb27
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-06 15:20:38 +0330

    implemented security filter JWTAuthFilter

---
+++ src/main/java/com/mmf/financeflow/security/JWTAuthFilter.java
@@ -0,0 +1,62 @@
+package com.mmf.financeflow.security;
+
+import com.mmf.financeflow.util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@AllArgsConstructor
+public class JWTAuthFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String AUTH_HEADER = request.getHeader("Authorization");
+        String username = null;
+        String token = null;
+
+        if (AUTH_HEADER != null && AUTH_HEADER.startsWith("Bearer ")) {
+            token = AUTH_HEADER.substring(7);
+
+            try {
+                username = jwtUtil.getUsernameFromToken(token);
+            } catch (ExpiredJwtException ex) {
+                System.out.println("Token is expired!");
+            } catch (Exception ex) {
+                System.out.println("Error while parsing the token!");
+            }
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtil.isTokenValid(token)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(username, null, userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
