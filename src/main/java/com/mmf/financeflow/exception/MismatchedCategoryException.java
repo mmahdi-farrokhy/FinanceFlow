ce6421f5458a24ce814c538191e3acf2f4904417
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 16:43:54 +0330

    created exception MismatchedCategoryException

---
+++ src/main/java/com/mmf/financeflow/exception/MismatchedCategoryException.java
@@ -0,0 +1,7 @@
+package com.mmf.financeflow.exception;
+
+public class MismatchedCategoryException extends RuntimeException {
+    public MismatchedCategoryException(String message) {
+        super(message);
+    }
+}
