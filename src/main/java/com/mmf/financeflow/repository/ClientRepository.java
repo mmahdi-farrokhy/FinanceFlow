36eafa30947ff123c28796e8033998eea9cfdc66
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 18:17:43 +0330

    added method to ClientRepository
          - findAccountsByUsername()
          - findIncomesByUsername()
          - findExpensesByUsername()
          - findBudgetsByUsername()

--- src/main/java/com/mmf/financeflow/repository/ClientRepository.java
+++ src/main/java/com/mmf/financeflow/repository/ClientRepository.java
@@ -1,12 +1,27 @@
 package com.mmf.financeflow.repository;
 
-import com.mmf.financeflow.entity.Client;
+import com.mmf.financeflow.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findByUsername(String username);
 
     boolean exitsByUsername(String username);
+
+    @Query("SELECT account FROM Account account WHERE account.client.username = :username")
+    List<Account> findAccountsByUsername(@Param("username") String username);
+
+    @Query("SELECT income FROM Income income WHERE income.client.username = :username")
+    List<Income> findIncomesByUsername(@Param("username") String username);
+
+    @Query("SELECT expense FROM Expense expense WHERE expense.client.username = :username")
+    List<Expense> findExpensesByUsername(@Param("username") String username);
+
+    @Query("SELECT budget FROM Budget budget WHERE budget.client.username = :username")
+    List<Budget> findBudgetsByUsername(@Param("username") String username);
 }
