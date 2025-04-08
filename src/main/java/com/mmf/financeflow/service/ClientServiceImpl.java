2e1910bce7d7cd5bbd336342cbca3783f7c0c65d
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 17:17:31 +0330

    renamed class AppUserServiceImpl to ClientServiceImpl

--- src/main/java/com/mmf/financeflow/service/ClientServiceImpl.java
+++ src/main/java/com/mmf/financeflow/service/ClientServiceImpl.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-public class AppUserServiceImpl implements ClientService {
+public class ClientServiceImpl implements ClientService {
     private ClientRepository appUserRepository;
     private AuthenticationManager authenticationManager;
     private UserDetailsService userDetailsService;
