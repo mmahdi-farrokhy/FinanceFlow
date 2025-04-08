28cd5b0ed317f2b5772045a796ae502953a3d5ec
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 14:44:35 +0330

    created exception DuplicatedAccountCategoryException

---
+++ src/main/java/com/mmf/financeflow/exception/DuplicatedAccountCategoryException.java
@@ -0,0 +1,7 @@
+package com.mmf.financeflow.exception;
+
+public class AccountCategoryDuplicationException extends RuntimeException {
+    public AccountCategoryDuplicationException(String message) {
+        super(message);
+    }
+}
