bd0e35f4d427c8f8bfae0a3c41cea1c021fe8412
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 16:44:05 +0330

    created exception InsufficientBalanceException

---
+++ src/main/java/com/mmf/financeflow/exception/InsufficientBalanceException.java
@@ -0,0 +1,7 @@
+package com.mmf.financeflow.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
