3fbb51e39eb6c24f63b351c966b299dea7b8abb4
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 18:18:50 +0330

    removed repository interface BudgetRepository

--- src/main/java/com/mmf/financeflow/repository/BudgetRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mmf.financeflow.repository;
-
-import com.mmf.financeflow.entity.Budget;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<Budget, Long> {
-}
