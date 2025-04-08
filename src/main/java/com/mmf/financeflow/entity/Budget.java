b79ac5bf9c722f60b623cc5ade8e5013e0652d00
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 14:11:39 +0330

    redesigned Budget entity

--- src/main/java/com/mmf/financeflow/entity/Budget.java
+++ src/main/java/com/mmf/financeflow/entity/Budget.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "budget")
@@ -15,10 +16,17 @@ public class Budget {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "amount", nullable = false)
+    private double amount;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "category", nullable = false)
-    private FinancialCategory category;
+    private BudgetCategory category;
 
-    @Column(name = "amount", nullable = false)
-    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
+    private AppUser appUser;
 }
