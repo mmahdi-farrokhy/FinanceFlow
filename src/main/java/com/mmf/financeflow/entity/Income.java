b0f1bdc695c0cdb8c2e8ed26897ac19c79051dc4
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-08 14:11:17 +0330

    redesigned Income entity

--- src/main/java/com/mmf/financeflow/entity/Income.java
+++ src/main/java/com/mmf/financeflow/entity/Income.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
@@ -17,15 +16,15 @@ public class Income {
     private Long id;
 
     @Column(name = "amount", nullable = false)
-    private BigDecimal amount;
-
-    @Column(name = "description", length = 255)
-    private String description;
+    private double amount;
 
     @Column(name = "date_time")
     private LocalDateTime dateTime;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "category", nullable = false)
-    private IncomeCategory category;
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "app_user_id")
+    private AppUser appUser;
 }
