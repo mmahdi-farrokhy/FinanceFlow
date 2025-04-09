1014fed3282db1df27e2de5e35f86e3442ce0322
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-09 13:43:17 +0330

    added abstract method createIncome() to ClientService

--- src/main/java/com/mmf/financeflow/service/ClientService.java
+++ src/main/java/com/mmf/financeflow/service/ClientService.java
@@ -1,10 +1,14 @@
 package com.mmf.financeflow.service;
 
+import com.mmf.financeflow.dto.IncomeRequest;
 import com.mmf.financeflow.dto.RegisterRequest;
 import com.mmf.financeflow.entity.Client;
+import com.mmf.financeflow.entity.Income;
 
 public interface ClientService {
     Client registerClient(RegisterRequest registerRequest);
 
     boolean existsByUsername(String username);
+
+    Income createIncome(IncomeRequest incomeRequest);
 }
