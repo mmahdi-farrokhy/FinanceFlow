f6a1d7f7fe0c744a52a14453faccc40aa7364b76
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-09 20:02:32 +0330

    added AuthenticationManager to SecurityConfig

--- src/main/java/com/mmf/financeflow/security/SecurityConfig.java
+++ src/main/java/com/mmf/financeflow/security/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.mmf.financeflow.repository.ClientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -38,5 +40,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
