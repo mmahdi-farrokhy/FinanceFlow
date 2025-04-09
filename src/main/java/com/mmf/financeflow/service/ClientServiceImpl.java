00a547c76e6be68f3c57c3df28a3757a9133347f
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-09 13:56:44 +0330

    implemented abstract method createIncome()

--- src/main/java/com/mmf/financeflow/service/ClientServiceImpl.java
+++ src/main/java/com/mmf/financeflow/service/ClientServiceImpl.java
@@ -1,9 +1,12 @@
 package com.mmf.financeflow.service;
 
+import com.mmf.financeflow.dto.IncomeRequest;
 import com.mmf.financeflow.dto.RegisterRequest;
 import com.mmf.financeflow.entity.Client;
+import com.mmf.financeflow.entity.Income;
 import com.mmf.financeflow.repository.ClientRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +27,15 @@ public class ClientServiceImpl implements ClientService {
     @Override
     public boolean existsByUsername(String username) {
         return clientRepository.existsByUsername(username);
+    }
+
+    @Override
+    public Income createIncome(IncomeRequest request, String username) {
+        Income income = new Income(request.getAmount(), request.getDescription());
+        Client client = clientRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User " + username + " not found!"));
+        client.addIncome(income);
+        clientRepository.save(client);
+        return income;
     }
 }
