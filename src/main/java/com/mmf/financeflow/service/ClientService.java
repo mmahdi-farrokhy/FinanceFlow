0df68aca3c051c8c5bffce4e36c0244b3ed67e2e
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-09 13:33:22 +0330

    removed abstract methods areLoginCredentialsValid() and loadUserByUsername() from interface ClientService

--- src/main/java/com/mmf/financeflow/service/ClientService.java
+++ src/main/java/com/mmf/financeflow/service/ClientService.java
@@ -1,17 +1,10 @@
 package com.mmf.financeflow.service;
 
-import com.mmf.financeflow.dto.JWTResponse;
-import com.mmf.financeflow.dto.LoginRequest;
 import com.mmf.financeflow.dto.RegisterRequest;
 import com.mmf.financeflow.entity.Client;
-import org.springframework.security.core.userdetails.UserDetails;
 
 public interface ClientService {
     Client registerClient(RegisterRequest registerRequest);
 
     boolean exitsByUsername(String username);
-
-    boolean areLoginCredentialsValid(LoginRequest loginRequest);
-
-    UserDetails loadUserByUsername(String username);
 }
--- src/main/java/com/mmf/financeflow/service/ClientServiceImpl.java
+++ src/main/java/com/mmf/financeflow/service/ClientServiceImpl.java
@@ -1,14 +1,9 @@
 package com.mmf.financeflow.service;
 
-import com.mmf.financeflow.dto.LoginRequest;
 import com.mmf.financeflow.dto.RegisterRequest;
 import com.mmf.financeflow.entity.Client;
 import com.mmf.financeflow.repository.ClientRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,38 +11,18 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ClientServiceImpl implements ClientService {
     private ClientRepository clientRepository;
-    private AuthenticationManager authenticationManager;
-    private UserDetailsService userDetailsService;
     private PasswordEncoder passwordEncoder;
 
     @Override
     public Client registerClient(RegisterRequest registerRequest) {
-        Client registeredClient = new Client();
-        registeredClient.setUsername(registerRequest.getUsername());
-        registeredClient.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        String username = registerRequest.getUsername();
+        String encryptedPassword = passwordEncoder.encode(registerRequest.getPassword());
+        Client registeredClient = new Client(username, encryptedPassword);
         return clientRepository.save(registeredClient);
     }
 
     @Override
     public boolean exitsByUsername(String username) {
         return clientRepository.existsByUsername(username);
-    }
-
-    @Override
-    public boolean areLoginCredentialsValid(LoginRequest loginRequest) {
-        try {
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
-            authenticationManager.authenticate(authenticationToken);
-
-            return true;
-        } catch (Exception ex) {
-            return false;
-        }
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String username) {
-        return userDetailsService.loadUserByUsername(username);
     }
 }
