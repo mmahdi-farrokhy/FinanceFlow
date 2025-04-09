61db958444dc7c8dd660f3851624407bed496182
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-09 13:49:32 +0330

    created DTO class AccountRequest

---
+++ src/main/java/com/mmf/financeflow/dto/AccountRequest.java
@@ -0,0 +1,17 @@
+package com.mmf.financeflow.dto;
+
+import com.mmf.financeflow.entity.BudgetCategory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountRequest {
+    private String title;
+    private BudgetCategory category;
+    private double balance;
+}
