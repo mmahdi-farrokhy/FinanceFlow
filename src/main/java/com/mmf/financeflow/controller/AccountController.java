a0aeb8f514e465a968ae0a1025acee737c73dac4
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-11 00:52:39 +0330

    replaced clientService with accountService in AccountController

--- src/main/java/com/mmf/financeflow/controller/AccountController.java
+++ src/main/java/com/mmf/financeflow/controller/AccountController.java
@@ -3,11 +3,9 @@ package com.mmf.financeflow.controller;
 import com.mmf.financeflow.dto.AccountRequest;
 import com.mmf.financeflow.entity.Account;
 import com.mmf.financeflow.entity.BudgetCategory;
-import com.mmf.financeflow.service.ClientService;
-import com.mmf.financeflow.util.SecurityUtil;
+import com.mmf.financeflow.service.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,23 +16,23 @@ import static com.mmf.financeflow.util.SecurityUtil.getUsernameFromAuthenticatio
 @RequestMapping("/api/account")
 @AllArgsConstructor
 public class AccountController {
-    private ClientService clientService;
+    private AccountService accountService;
 
     @PostMapping("")
     public ResponseEntity<Account> createAccount(@RequestBody AccountRequest request) {
         String username = getUsernameFromAuthenticationContext();
-        return ResponseEntity.ok(clientService.createAccount(request, username));
+        return ResponseEntity.ok(accountService.createAccount(request, username));
     }
 
     @GetMapping("")
     public ResponseEntity<List<Account>> getAccounts() {
         String username = getUsernameFromAuthenticationContext();
-        return ResponseEntity.ok(clientService.getAccounts(username));
+        return ResponseEntity.ok(accountService.getAccounts(username));
     }
 
     @GetMapping("/category")
-    public ResponseEntity<Account> getAccountByType(@RequestParam BudgetCategory category){
+    public ResponseEntity<Account> getAccountByType(@RequestParam BudgetCategory category) {
         String username = getUsernameFromAuthenticationContext();
-        return ResponseEntity.ok(clientService.getAccountByType(username, category));
+        return ResponseEntity.ok(accountService.getAccountByType(username, category));
     }
 }
