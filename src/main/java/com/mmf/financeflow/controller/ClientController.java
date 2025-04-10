4792bedf38250a8bb0824d708b4f1aec15536d3a
mmahdi-farrokhy <mmahdifarrokhy@gmail.com>
2025-04-10 22:13:30 +0330

    developed controller method getIncomes() in ClientController

--- src/main/java/com/mmf/financeflow/controller/ClientController.java
+++ src/main/java/com/mmf/financeflow/controller/ClientController.java
@@ -10,10 +10,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/client")
@@ -53,6 +52,12 @@ public class ClientController {
     public ResponseEntity<Account> createAccount(@RequestBody AccountRequest request) {
         String username = getUsernameFromAuthenticationContext();
         return ResponseEntity.ok(clientService.createAccount(request, username));
+    }
+
+    @GetMapping("/income")
+    public ResponseEntity<List<Income>> getIncomes() {
+        String username = getUsernameFromAuthenticationContext();
+        return ResponseEntity.ok(clientService.getIncomes(username));
     }
 
     private static String getUsernameFromAuthenticationContext() {
